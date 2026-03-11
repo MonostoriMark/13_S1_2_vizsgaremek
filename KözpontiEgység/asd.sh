5a0b2f4e8698e72ce0a27bdf49ec96c89aa758a5
MonostoriMark <monostorimark05@gmail.com>
2026-03-11 13:53:11 +0100

    teszt1

---
+++ KözpontiEgység/asd.sh
@@ -0,0 +1,3 @@
+lxterminal -e "sudo systemctl status hotelFlowCardValidator.service" &
+lxterminal -e "sudo systemctl status hotelFlowLocalRoutes.service" &
+lxterminal -e "sudo systemctl status hotelFlowEntering.service" &
